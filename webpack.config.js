36cee7a75b6b89ecc3fccf34dd395e919cab54f0
Mohamed Emad <m.emad550@gmail.com>
2019-03-23 18:54:04 +0200

    add cache busting and html webpack plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,27 @@
 const path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin')
 module.exports = {
     mode: "development",
     entry: "./src/index.js",
     output: {
-        filename: 'main.js',
+        filename: 'main.[contentHash].js',
         path: path.resolve(__dirname, 'dist')
     },
+    plugins:[
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        })
+    ],
     module: {
-        rules: [{
-            test: /\.s[ac]ss$/,
-            use: [{
-                loader: "style-loader"
-            }, {
-                loader: "css-loader"
-            }, {
-                loader: "sass-loader",
-            }]
-        }, ]
+        rules: [
+            {
+                test: /\.s[ac]ss$/,
+                use: [
+                    "style-loader",
+                    "css-loader",
+                    "sass-loader"
+                ]
+            },
+        ]
     }
 }
