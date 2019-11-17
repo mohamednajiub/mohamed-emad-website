c7f059eb52763a1d615a7a1d219e5ba1dc07faf4
mohamednajiub <m.emad550@gmail.com>
2019-11-17 12:49:14 +0200

    add single.php

--- index.php
+++ index.php
@@ -1,1 +1,21 @@
-this is My first full wordpress theme
+
+<h1>
+    <?php
+        bloginfo('name');
+    ?>
+</h1>
+<p>
+    <?php bloginfo('description'); ?>
+</p>
+<?php
+    while(have_posts()){
+        the_post();?>
+        <h3>
+            <a href="<?php the_permalink(); ?>">
+                <?php the_title(); ?>
+            </a>
+        </h3>
+    <?php 
+    the_content();
+    };
+?>
