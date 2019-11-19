7385483886b4a0097d189cb056b0b69271e53cc7
mohamednajiub <m.emad550@gmail.com>
2019-11-19 19:24:23 +0200

    change title

--- header.php
+++ header.php
@@ -5,7 +5,7 @@
     <meta charset="<?php bloginfo("charset"); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Mohamed Najiub</title>
     <?php wp_head(); ?>
 </head>
 
