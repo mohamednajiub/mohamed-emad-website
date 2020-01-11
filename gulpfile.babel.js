9cde73597903680f5f8fd8abfc7f469c2beaab7d
mohamednajiub <m.emad550@gmail.com>
2020-01-11 15:42:32 +0200

    add gulp workflow

---
+++ gulpfile.babel.js
@@ -0,0 +1,21 @@
+import { src, dest } from 'gulp';
+import yargs from 'yargs';
+import sass from 'gulp-sass';
+import cleanCss from 'gulp-clean-css';
+import gulpif from 'gulp-if';
+import postcss from 'gulp-postcss';
+import sourcemaps from 'gulp-sourcemaps';
+import autoprefixer from 'autoprefixer';
+
+const PRODUCTION = yargs.argv.prod;
+
+
+export const styles = () => {
+    return src('./scss/style.scss')
+        .pipe(gulpif(!PRODUCTION, sourcemaps.init()))
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulpif(PRODUCTION, postcss([ autoprefixer ])))
+        .pipe(gulpif(PRODUCTION, cleanCss({compatibility:'ie8'})))
+        .pipe(gulpif(!PRODUCTION, sourcemaps.write()))
+        .pipe(dest('./'));
+}
