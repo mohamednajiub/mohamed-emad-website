4cb3fe411a40f2b6e0ffd8dcc5d46893728c436b
mohamednajiub <m.emad550@gmail.com>
2020-02-01 14:50:16 +0200

    add: add navbar structure

--- front-page.php
+++ front-page.php
@@ -1,18 +1,2 @@
 <?php get_header() ?>
-<h1>
-    <?php bloginfo('name'); ?>
-</h1>
-<p><?php bloginfo('description'); ?></p>
-<?php
-while (have_posts()) {
-    the_post(); ?>
-    <h3>
-        <a href="<?php the_permalink(); ?>">
-            <?php the_title(); ?>
-        </a>
-    </h3>
-<?php
-    the_content();
-};
-get_footer();
-?>
+<?php get_footer(); ?>
--- header.php
+++ header.php
@@ -9,3 +9,24 @@
 </head>
 
 <body>
+
+<nav>
+    <div class="container">
+        <div class="row justify-content-between">
+            <div class="col-md-3">
+                <a href="<?php echo site_url() ?>" title="Mohamed Najiub">
+                    <h1>Mohamed Najiub</h1>
+                    <span>Front-end/WordPress Developer</span>
+                </a>
+            </div>
+            <div class="col-md-6">
+                <ul class="d-flex justify-content-between">
+                    <li><a href="#" title="about">About</a></li>
+                    <li><a href="#" title="about">Portfolio</a></li>
+                    <li><a href="#" title="about">Blog</a></li>
+                    <li><a href="#" title="about">Contact</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</nav>
