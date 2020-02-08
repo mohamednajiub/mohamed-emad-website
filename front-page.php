af78b58d86e272515b2743b3d737eb3cc7bb4e2b
mohamednajiub <m.emad550@gmail.com>
2020-02-08 19:42:14 +0200

    add front-page header

--- front-page.php
+++ front-page.php
@@ -1,2 +1,16 @@
 <?php get_header() ?>
+<header class="container">
+    <div class="row">
+        <div class="col-sm-6 photo">
+            <img src="<?php echo get_template_directory_uri(); ?>/images/Mohamed-Najiub.jpg" alt="Mohamed Najiub Picture">
+        </div>
+        <div class="col-sm-6 content">
+            <p>As an Accountant turned Front-end, Najiub has developed a unique perspective when it comes to
+                capturing the story behind his work. Mohamed Najiub is a Front-end developer who is committed to
+                quality and teaching. He has spent the last 3 years creating a portfolio, volunteering work and
+                sharing the experience. is currently most passionate about office work.
+            </p>
+        </div>
+    </div>
+</header>
 <?php get_footer(); ?>
