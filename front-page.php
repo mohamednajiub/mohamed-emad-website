eca2ead1f1c72bc8189a443956d7b64e5034cf63
mohamednajiub <m.emad550@gmail.com>
2020-02-09 08:08:53 +0200

    add header section

--- front-page.php
+++ front-page.php
@@ -1,15 +1,22 @@
 <?php get_header() ?>
-<header class="container">
-    <div class="row">
-        <div class="col-sm-6 photo">
-            <img src="<?php echo get_template_directory_uri(); ?>/images/Mohamed-Najiub.jpg" alt="Mohamed Najiub Picture">
-        </div>
-        <div class="col-sm-6 content">
-            <p>As an Accountant turned Front-end, Najiub has developed a unique perspective when it comes to
-                capturing the story behind his work. Mohamed Najiub is a Front-end developer who is committed to
-                quality and teaching. He has spent the last 3 years creating a portfolio, volunteering work and
-                sharing the experience. is currently most passionate about office work.
-            </p>
+<header class="my-4">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-6 photo">
+                <img src="<?php echo get_template_directory_uri(); ?>/images/Mohamed-Najiub.jpg" alt="Mohamed Najiub Picture">
+            </div>
+            <div class="col-sm-6 details">
+                <h1>Mohamed Najiub</h1>
+                <p>As an Accountant turned Front-end, Najiub has developed a unique perspective when it comes to
+                    capturing the story behind his work. Mohamed Najiub is a Front-end developer who is committed to
+                    quality and teaching. He has spent the last 3 years creating a portfolio, volunteering work and
+                    sharing the experience. is currently most passionate about office work.
+                </p>
+                <div>
+                    <a href=""></a>
+                    <a href=""></a>
+                </div>
+            </div>
         </div>
     </div>
 </header>
