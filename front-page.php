39966a1e3dc63abb374b7ca051987a573c3528b3
mohamednajiub <m.emad550@gmail.com>
2020-02-14 18:42:27 +0200

    finish header section

--- front-page.php
+++ front-page.php
@@ -2,17 +2,17 @@
 <header class="my-4">
     <div class="container">
         <div class="row">
-            <div class="col-sm-6 photo">
+            <div class="col-sm-6 header--photo">
                 <img src="<?php echo get_template_directory_uri(); ?>/images/Mohamed-Najiub.jpg" alt="Mohamed Najiub Picture">
             </div>
-            <div class="col-sm-6 details">
+            <div class="col-sm-6 header--details">
                 <h1>Mohamed Najiub</h1>
                 <p>As an Accountant turned Front-end, Najiub has developed a unique perspective when it comes to
                     capturing the story behind his work. Mohamed Najiub is a Front-end developer who is committed to
                     quality and teaching. He has spent the last 3 years creating a portfolio, volunteering work and
                     sharing the experience. is currently most passionate about office work.
                 </p>
-                <div>
+                <div class="header--footer">
                     <a href="" class="btn btn--primary">Resume</a>
                     <a href="" class="btn btn--default">contact </a>
                 </div>
