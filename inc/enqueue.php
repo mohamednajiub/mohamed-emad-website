13c31081750aab662edfb58e3cf9a7788ccf47ca
mohamednajiub <m.emad550@gmail.com>
2020-03-14 01:16:48 +0200

    remove debuging echo

--- inc/enqueue.php
+++ inc/enqueue.php
@@ -10,7 +10,6 @@
 
 function najiub_load_admin_scripts($hook)
 {
-    echo $hook;
     if( $hook != 'toplevel_page_najiub' ) { return; }
 
     wp_register_style( 'najiub_admin_styles', get_template_directory_uri() . '/css/najiub_admin.css', array(), '1.0.0', 'all' );
