8ba0399106b5b94fb25c0f7c215efe063c54c9bb
Mohamed Najiub <mohamednajiub@gmail.com>
2020-03-20 20:02:04 +0200

    style: style social icons in footer

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 <footer>
-    <div class="container">
+    <div class="container d-flex justify-content-between align-items-center">
         <p>
             © Mohamed Najiub - <?php echo date('Y'); ?>
         </p>
@@ -9,7 +9,6 @@
                 $social_media = array('github', 'linkedin', 'medium', 'twitter', 'facebook', 'instagram');
                 foreach ($social_media as $link):
                     if(get_option($link)):
-
                 ?>
                     <li><a href="<?php echo get_option($link)?>"><i class="fab fa-<?php echo $link ?>"></i></a></li>
                 <?php
