299dfd62f6b65d97cab8ae87c1890ed7c39bef33
Mohamed Najiub <mohamednajiub@gmail.com>
2020-03-27 02:31:39 +0200

    fix nav in mobile

--- header.php
+++ header.php
@@ -13,21 +13,21 @@
 <nav>
     <div class="container">
         <div class="row justify-content-between">
-            <div class="col-md-4 brand">
+            <div class="col-lg-4 brand">
                 <a href="<?php echo site_url(); ?>" title="Mohamed Najiub" class="brand--link">
                     <h1 class="m-0">Mohamed Najiub</h1>
                     <span>Front-end & WordPress Developer</span>
                 </a>
             </div>
-            <div class="col-md-6 navigation">
-                <ul class="d-flex flex-column flex-md-row  justify-content-between flex-wrap">
+            <div class="col-lg-6 navigation">
+                <ul class="d-flex flex-column flex-lg-row justify-content-between p-0 align-items-center">
                     <li><a href="#" title="about">About</a></li>
                     <li><a href="#" title="about">Portfolio</a></li>
                     <li><a href="#" title="about">Blog</a></li>
                     <li><a href="#" title="about">Contact</a></li>
                 </ul>
             </div>
-            <div class="menu--toggler d-flex d-md-none flex-column justify-content-between" id="menu_toggler">
+            <div class="menu--toggler d-flex d-lg-none flex-column justify-content-between" id="menu_toggler">
                 <div class="bar"></div>
                 <div class="bar"></div>
                 <div class="bar"></div>
