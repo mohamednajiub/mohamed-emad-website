2d540fdd0388c43ebd7a5c4742ccbb41614f02f7
Mohamed Najiub <mohamednajiub@gmail.com>
2020-05-26 03:57:14 +0200

    edit index page

--- index.php
+++ index.php
@@ -19,10 +19,11 @@ get_header();
 <?php
 if (is_home() && !is_front_page()) :
 ?>
-	<header style="background-image: url('<?php echo has_post_thumbnail() ? get_the_post_thumbnail_url() : 'https://via.placeholder.com/1220x400' ?>')">
+	<header class="page--header" style="background-image: url('<?php echo has_post_thumbnail() ? get_the_post_thumbnail_url() : 'https://via.placeholder.com/1220x400' ?>')">
+		<div class="overlay"></div>
 		<div class="container">
 			<div class="row justify-content-center align-items-center align-content-center">
-				<h3><?php single_post_title() ?></h3>
+				<h2 class="page--title"><?php single_post_title() ?></h2>
 			</div>
 		</div>
 	</header>
@@ -33,30 +34,30 @@ endif;
 <div class="section">
 	<div class="container">
 		<div class="row justify-content-center align-items-center flex-wrap">
-		<?php
-		if ( have_posts() ) :
+			<?php
+			if (have_posts()) :
 
-			/* Start the Loop */
-			while ( have_posts() ) :
-				the_post();
+				/* Start the Loop */
+				while (have_posts()) :
+					the_post();
 
-				/*
+					/*
 				 * Include the Post-Type-specific template for the content.
 				 * If you want to override this in a child theme, then include a file
 				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
 				 */
-				get_template_part( 'template-parts/content', get_post_type() );
+					get_template_part('template-parts/content', get_post_type());
 
-			endwhile;
+				endwhile;
 
-			the_posts_navigation();
+				the_posts_navigation();
 
-		else :
+			else :
 
-			get_template_part( 'template-parts/content', 'none' );
+				get_template_part('template-parts/content', 'none');
 
-		endif;
-		?>
+			endif;
+			?>
 		</div>
 	</div>
 </div>
