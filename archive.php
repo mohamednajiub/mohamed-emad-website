17401ff0462dec339cd7aaeb3d0d903e9caa4cf8
Mohamed Najiub <mohamednajiub@gmail.com>
2020-05-27 05:06:52 +0200

    add pagination

--- archive.php
+++ archive.php
@@ -31,22 +31,41 @@ get_header();
 			</div>
 		</div>
 	</header><!-- .page-header -->
+	<main>
+		<section>
+			<div class="container">
+				<div class="row">
+					<?php
+					/* Start the Loop */
+					while (have_posts()) :
+						the_post();
+						get_template_part('template-parts/content', get_post_type());
+					endwhile;
+					// the_posts_navigation();
+					?>
 
+				</div>
+			</div>
+		</section>
+
+
+		<section class="pagination">
+			<div class="container">
+				<div class="row justify-content-center align-items-center">
+					<?php
+					the_posts_pagination(
+						array(
+							'screen_reader_text' => ' ',
+							'prev_text' => '<i class="fas fa-chevron-left"></i>',
+							'next_text' => '<i class="fas fa-chevron-right"></i>'
+						)
+					);
+					?>
+				</div>
+			</div>
+		</section>
+	</main>
 <?php
-	/* Start the Loop */
-	while (have_posts()) :
-		the_post();
-
-		/*
-	 * Include the Post-Type-specific template for the content.
-	 * If you want to override this in a child theme, then include a file
-	 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
-	 */
-		get_template_part('template-parts/content', get_post_type());
-
-	endwhile;
-
-	the_posts_navigation();
 
 else :
 
--- index.php
+++ index.php
@@ -31,35 +31,52 @@ if (is_home() && !is_front_page()) :
 endif;
 ?>
 
-<div class="section">
-	<div class="container">
-		<div class="row justify-content-center align-items-center flex-wrap">
-			<?php
-			if (have_posts()) :
+<main>
+	<section>
+		<div class="container">
+			<div class="row justify-content-center align-items-center flex-wrap">
+				<?php
+				if (have_posts()) :
 
-				/* Start the Loop */
-				while (have_posts()) :
-					the_post();
+					/* Start the Loop */
+					while (have_posts()) :
+						the_post();
 
-					/*
+						/*
 				 * Include the Post-Type-specific template for the content.
 				 * If you want to override this in a child theme, then include a file
 				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
 				 */
-					get_template_part('template-parts/content', get_post_type());
+						get_template_part('template-parts/content', get_post_type());
 
-				endwhile;
+					endwhile;
 
-				the_posts_navigation();
+				else :
 
-			else :
+					get_template_part('template-parts/content', 'none');
 
-				get_template_part('template-parts/content', 'none');
-
-			endif;
-			?>
+				endif;
+				?>
+			</div>
 		</div>
-	</div>
-</div>
+	</section>
+
+	<section class="pagination">
+		<div class="container">
+			<div class="row justify-content-center align-items-center">
+				<?php
+				the_posts_pagination(
+					array(
+						'screen_reader_text' => ' ',
+						'prev_text' => '<i class="fas fa-chevron-left"></i>',
+						'next_text' => '<i class="fas fa-chevron-right"></i>'
+					)
+				);
+				?>
+			</div>
+		</div>
+	</section>
+</main>
+
 
 <?php get_footer(); ?>
